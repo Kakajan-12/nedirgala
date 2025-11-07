0e4bd14876a8a1c56032acd44d456ef070c88b79
Bashim Owezgulyyew <82506614+bashim-ovezguly@users.noreply.github.com>
2025-11-07 18:34:51 +0500

    Delete .next directory

--- .next/dev/static/chunks/src_app_product_base_page_e456d7f8.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_96e465f5._.js",
-  "static/chunks/src_app_product_base_page_1b1d495b.js",
-  "static/chunks/public_assets_css_fancybox_24011da9.css"
-],
-    source: "dynamic"
-});
